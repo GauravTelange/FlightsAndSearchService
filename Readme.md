856f740ae716a2bb51d2304286dbf84559a76c66
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-07 01:36:28 +0530

    airport migrationa and seed completed

--- Readme.md
+++ Readme.md
@@ -36,3 +36,16 @@ and then execute `npx sequelize db:migrate`
 - A Flight belong to an airplane but one airplane can be used in mulitple Flights
 - A City has many airports but one airport belongs to a city
 - one Airport can have many flights, but a flight belongs to a one airport
+
+
+## Tables
+
+###  City -> id, name, created_at, updated_at
+### Airport -> id, name, address, city_id, created_at, updated_at
+     Relationship -> City has many airports and Airport belongs to a city(one to many)
+
+
+  ```
+  $ npx sequelize model:generate --name Airport --attributes name:String,address:String,cityId:integer
+  
+  ```    
---
+++ src/seeders/20260406195219-add-airports.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const airport = require('../models/airport');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airports', [
+      {
+        name: 'CSMI Airport',
+        cityId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'JFK Airport',
+        cityId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Indira Gandhi International Airport',
+        cityId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Heathrow Airport',
+        cityId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date() 
+      }
+    ], {});  
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
