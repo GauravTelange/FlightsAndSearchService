67dee88695bb2999cd9f6b8c9f3134cc43ddf219
Gaurav Telange <gauravtelange4321@gmail.com>
2026-04-07 20:52:31 +0530

    added airplanes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { PORT } = require('./config/serverConfig');
 const APIRoutes = require('./routes/index');
 
 const db  = require('./models/index');
-
+const {Airplane} = require('./models/index');
 //const { City, Airport } = require('./models/index');
 const { where } = require("sequelize");
 const setupandAndStartServer = async () =>  {
@@ -27,6 +27,10 @@ const setupandAndStartServer = async () =>  {
             await db.sequelize.sync({ alter: true });
             
         }
+        await Airplane.create({
+            modelNumber: 'VIshjit 420',
+            
+        });
 
 
         
